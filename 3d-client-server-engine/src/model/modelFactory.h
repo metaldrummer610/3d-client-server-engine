e4fec6cb48604c7ed648667febeb360eb5eb084e
metaldrummer610@gmail.com <metaldrummer610@gmail.com@6b4c4cb7-6256-0410-809d-5d0480a18c6e>
2008-09-13 15:23:23 +0000

    Merged from branch Test r.37 to main

--- 3d-client-server-engine/src/model/modelFactory.h
+++ 3d-client-server-engine/src/model/modelFactory.h
@@ -5,15 +5,12 @@
 #include "abstractModel.h"
 #include "cube.h"
 #include "pyramid.h"
-
 #include "wavefront/loader.h"
 
 class ModelFactory {
 private:
-	std::string sphereFilePath;
-	WavefrontLoader wfLoader;
+	Loader l;
 public:
-	ModelFactory();
 	AbstractModel* getModel(std::string s);
 	AbstractModel* getModelByName(std::string name);
 };
